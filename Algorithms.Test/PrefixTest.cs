4bdd70157c9284c16da945ec54e110066cce9818
Artem Leonov <leonovru@yandex.ru>
2019-12-02 00:09:04 +0300

    Added new class 'Prefix'

---
+++ Algorithms.Test/PrefixTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Test
+{
+    [TestClass]
+    public class PrefixTest
+    {
+        [TestMethod]
+        public void CalculateFunctionTest1()
+        {
+            var expected = new int[] { 0, 0, 0, 1, 2, 3, 4 };
+            var actual = Prefix.CalculateFunction("abcabca");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateFunctionTest12()
+        {
+            var expected = new int[] { 0, 0, 0, 1, 1, 2, 3, 4, 2 };
+            var actual = Prefix.CalculateFunction("abbaabbab");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateFunctionTest3()
+        {
+            var expected = new int[] { 0, 0, 0, 1, 2, 3, 0 };
+            var actual = Prefix.CalculateFunction("abcabcd");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Prefix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Prefix
+    {
+        public static int[] CalculateFunction(string input)
+        {
+            int[] pi = new int[input.Length];
+            for (int i = 1; i < input.Length; i++)
+            {
+                int k = pi[i - 1];
+                while (k > 0 && input[k] != input[i])
+                    k = pi[k - 1];
+
+                if (input[k] == input[i])
+                    k++;
+
+                pi[i] = k;
+            }
+
+            return pi;
+        }
+    }
+}
