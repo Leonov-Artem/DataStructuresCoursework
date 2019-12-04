7cbe543b299950d18d0318c7bb2dab6a6f8bfd77
Artem Leonov <leonovru@yandex.ru>
2019-12-05 01:53:44 +0300

    Added some changes

---
+++ Algorithms.Test/BoyerMooreTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Test
+{
+    [TestClass]
+    public class BoyerMooreTest
+    {
+        [TestMethod]
+        public void Find_OrdinaryPatternAndText_1PositionsReturned()
+        {
+            string text = "GCATCGCAGAGAGTATACAGTACG";
+            string pattern = "GCAGAGAG";
+
+            var expected = new List<int>() { 5 };
+            var actual = BoyerMoore.Find(text, pattern);
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Find_SearchNonexistentSubstring_EmptyListReturned()
+        {
+            string text = "abcde";
+            string pattern = "ghf";
+
+            var expected = new List<int>();
+            var actual = BoyerMoore.Find(text, pattern);
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Find_MultiplePatternOccurrences_3PositionsReturned()
+        {
+            string text = "hi adb hi 48 hiabc";
+            string pattern = "hi";
+
+            var expected = new List<int>() { 0, 7, 13 };
+            var actual = BoyerMoore.Find(text, pattern);
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Find_OnlyIdenticalCharactersInText_4PositionsReturned()
+        {
+            string text = "aaaa";
+            string pattern = "a";
+
+            var expected = new List<int>() { 0, 1, 2, 3 };
+            var actual = BoyerMoore.Find(text, pattern);
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Find_PatternWithWhiteSpaces_1PositionsReturned()
+        {
+            string text = "sd bd de hello world 53 5 sdf";
+            string pattern = "hello world";
+
+            var expected = new List<int>() { 9 };
+            var actual = BoyerMoore.Find(text, pattern);
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Find_DifferentCases_EmptyListReturned()
+        {
+            string text = "abd Hello world adt";
+            string pattern = "hello world";
+
+            var expected = new List<int>();
+            var actual = BoyerMoore.Find(text, pattern);
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
--- Algorithms/BoyerMoore.cs
+++ Algorithms/BoyerMoore.cs
@@ -11,28 +11,29 @@ namespace Algorithms
         public static List<int> Find(string text, string pattern)
         {
             int MAX_POSSIBLE_SHIFT = text.Length - pattern.Length;
-            var foundShifts = new List<int>();
+            var shifts = new List<int>();
 
             var badCharacter = ComputeBadCharacter(pattern);
             int[] goodSuffix = ComputeGoodSuffix(pattern);
-            int shift = 0;
+            int currentShift = 0;
 
-            while (shift <= MAX_POSSIBLE_SHIFT)
+            while (currentShift <= MAX_POSSIBLE_SHIFT)
             {
-                int j = pattern.Length - 1;
-                while (j > 0 && pattern[j] == text[shift + j])
+                int j = pattern.Length;
+                while (j > 0 && pattern[j - 1] == text[currentShift + j - 1])
                     j--;
 
                 if (j == 0)
                 {
-                    foundShifts.Add(shift);
-                    shift += goodSuffix[0];
+                    shifts.Add(currentShift);
+                    currentShift += goodSuffix[0];
                 }
                 else
-                    shift += Math.Max(goodSuffix[j], j - badCharacter[text[shift + j]]);
+                    currentShift += Math.Max(goodSuffix[j - 1], 
+                        j - badCharacter[text[currentShift + j - 1]]);
             }
 
-            return foundShifts;
+            return shifts;
         }
 
         private static BadCharacterHeuristic ComputeBadCharacter(string pattern)
